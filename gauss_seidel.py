bc513163ea6fdc69a498223e7d49b44c76316e1b
Jordi Bardales <jbardalesr@uni.pe>
2022-05-04 16:54:35 -0500

    Jacobi mathod

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -22,6 +22,27 @@ def gauss_seidel(A, b, x0, MAX_ITER=100, tol=1e-5):
     print("Too many iterations")
 
 
+def gauss_seidel_accel(A, b, x0, omega, MAX_ITER=100, tol=0.0003):
+    n = len(A[0])
+    x = np.zeros(n)
+    print(f"x[0] = {x0}")
+
+    for k in range(1, MAX_ITER):
+        for i in range(n):
+            s1 = np.dot(A[i, :i], x[:i])
+            s2 = np.dot(A[i, i + 1:], x0[i + 1:])
+            z = (b[i] - s1 - s2) / A[i, i]
+            x[i] = omega*z + (1 - omega)*x0[i]
+
+        print(f"x[{k}] = {x}")
+
+        if la.norm(x - x0, np.inf) < tol*la.norm(x0, np.inf):
+            return x, k
+
+        x0 = np.copy(x)
+    print("Too many iterations")
+
+
 A = np.array([[10, 3, 1],
               [2, -10, 3],
               [1, 3, 10.]])
@@ -31,4 +52,4 @@ b = np.array([14.0, -5, 14])
 x0 = np.array([0.0, 0.0, 0.0])
 
 print("Gauss Seidel method")
-gauss_seidel(A, b, x0)
+gauss_seidel_accel(A, b, x0, omega=0.9)
---
+++ jacobi.jl
@@ -0,0 +1,49 @@
+using LinearAlgebra
+using Printf
+
+const la = LinearAlgebra
+
+
+const MAX_ITER = 100
+
+function summation(a_i, x, i)
+    sum_i = 0.0
+    for j=1:size(x)[1]
+        if i != j
+            sum_i += a_i[j]*x[j]
+        end
+    end
+    return sum_i
+end
+
+function jacobi_method(A::Matrix{Float64}, b, x0, tol=1e-5)
+    m, n = size(A)
+    x = zeros(Float64, m)
+
+    k = 1
+    println("x[0] = $x0")
+    while k < MAX_ITER
+        for i=1:m
+            x[i] = (b[i] - summation(A[i, :], x0, i))/A[i, i]
+        end
+        println("x[$k] = $x")
+
+        if la.norm(x .- x0) < tol*la.norm(x)
+            break
+        end
+        x0 = copy(x) 
+        k += 1
+    end
+end
+
+A = [10 3 1;
+     2 -10 3;
+     1 3 10.]
+
+b = [14.0 -5 14]
+
+x0 = [0.0 0.0 0.0]
+
+println("Jacobi method")
+jacobi_method(A, b, x0)
+
