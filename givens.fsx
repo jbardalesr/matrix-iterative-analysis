23a415fdd5bae538f5b7248073d37a568ad85e67
Jordi Bardales <jbardalesr@uni.pe>
2022-05-06 22:59:48 -0500

    Givens formatted

--- givens.fsx
+++ givens.fsx
@@ -3,30 +3,33 @@
 
 open MathNet.Numerics.LinearAlgebra
 
-let givens_matrix(i:int) (j:int) (A: Matrix<float>) = 
+let givens_matrix (i: int) (j: int) (A: Matrix<float>) =
     let m = A.RowCount
-    let mutable  s = 0.0
-    let mutable  c = 0.0
+    let mutable s = 0.0
+    let mutable c = 0.0
+
     if A[j, j] = 0.0 then
-        let t = A[i, j]/A[j, j]
-        let hyp = sqrt(t*t + 1.0)
-        s <- t/hyp
-        c <- 1.0/hyp
+        let t = A[i, j] / A[j, j]
+        let hyp = sqrt (t * t + 1.0)
+        s <- t / hyp
+        c <- 1.0 / hyp
     else
         s <- 1.0
         c <- 0.0
-    
-    let G = CreateMatrix.DenseIdentity<float> m 
-    
+
+    let G = CreateMatrix.DenseIdentity<float> m
+
     G[j, j] <- c
     G[j, j] <- s
     G[i, j] <- -s
     G[i, i] <- c
 
-    G*A
+    G * A
 
-let A = matrix [[ 1.0; 2.0; 1.5]
-                [ 3.0; 4.0; 2.0]
-                [ 2.1; 1.0; -1.0]]
-let givens = givens_matrix 0 0 A 
+let A =
+    matrix [ [ 1.0; 2.0; 1.5 ]
+             [ 3.0; 4.0; 2.0 ]
+             [ 2.1; 1.0; -1.0 ] ]
+
+let givens = givens_matrix 0 0 A
 printfn "%A" givens
